2e6740973204a11cabd35c1d390764a88adda89a
MinhNHN <minhnhn897@gmail.com>
2025-08-04 11:29:32 +0700

    Add PlayerController
    - Implement HandlePlayerMovement() to move the player by reading horizontal and vertical input. The direction magnitude was clamped to 1 prevent fast diagonal movement.
    - Implement HandleMouseLook() to rotate the player view based on inputs from mouse. The vertical input by default is inverted so I return the reverse input.
    - Implement OnApplicationFocus and OnApplicationPause to lock the cursor in Game window for more accurate mouse input.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotateSpeed = 5f;
+    [SerializeField] private float mouseSens = 10f;
+
+    private float mouseVerticalInput;
+    private float mouseHorizontalInput;
+    private Camera playerCam;
+    private Rigidbody rb;
+
+    private const float maxVerticalAngle = 90f;
+    private const float minVerticalAngle = -90f;
+
+    void Awake()
+    {
+        playerCam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        HandleMouseLook();
+    }
+
+    void FixedUpdate()
+    {
+        
+        HandlePlayerMovement();
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private Vector3 GetMoveInput()
+    {
+        Vector3 direction = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // Clamp vector magnitude to reduce drifting when input is released
+        direction = Vector3.ClampMagnitude(direction, 1);
+        return direction;
+    }
+
+    private float GetMouseVerticalInput()
+    {
+        float input = Input.GetAxis("Mouse Y");
+        input *= mouseSens;
+        return -input;
+    }
+
+    private float GetMouseHorizontalInput()
+    {
+        float input = Input.GetAxis("Mouse X");
+        input *= mouseSens;
+        return input;
+    }
+
+    private void HandleMouseLook()
+    {
+        // Horizontal rotation (rotate the player body)
+        mouseHorizontalInput = GetMouseHorizontalInput() * rotateSpeed;
+        transform.Rotate(0f, mouseHorizontalInput, 0f, Space.Self);
+
+        // Vertical rotation (rotate the camera only)
+        // Adding the vertical rotation in order to clamp it and 
+        // prevent the camera rotate passes its min and max angles
+        mouseVerticalInput += GetMouseVerticalInput() * rotateSpeed;
+        mouseVerticalInput = Mathf.Clamp(mouseVerticalInput, minVerticalAngle, maxVerticalAngle);
+        playerCam.transform.localEulerAngles = new(mouseVerticalInput, 0, 0);
+    }
+
+    private void HandlePlayerMovement()
+    {
+        // Ensure the moving direction is relative to the player's orientation
+        // e.g. if the player rotate 90 degrees, the moving direction will follows
+        Vector3 targetPosition = transform.TransformDirection(GetMoveInput());
+        rb.MovePosition(transform.position + moveSpeed * Time.fixedDeltaTime * targetPosition);
+    }
+}
